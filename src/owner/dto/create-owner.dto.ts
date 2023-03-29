a5bddfb7da76a05f2a1f8040657ce3a9a382d2a9
Your Name <you@example.com>
2023-03-29 12:27:18 +0200

    Create owner dto

--- src/owner/dto/create-owner.dto.ts
+++ src/owner/dto/create-owner.dto.ts
@@ -1,1 +1,13 @@
-export class CreateOwnerDto {}
+/* eslint-disable prettier/prettier */
+
+import { IsDefined } from "class-validator";
+
+export default class CreateOwner {
+
+  @IsDefined({message: 'A név megadása kötelező'})
+  fullName: string;
+
+  @IsDefined({message: 'A tulajdonos típusa megadása kötelező'})
+  business: boolean;
+
+}
