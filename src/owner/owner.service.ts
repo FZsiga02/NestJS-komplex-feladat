83cc1cfdb94782f50159d35f2352b66561966c95
Your Name <you@example.com>
2023-03-29 12:36:48 +0200

    Create new owner

--- src/owner/owner.service.ts
+++ src/owner/owner.service.ts
@@ -1,11 +1,21 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-import { CreateOwnerDto } from './dto/create-owner.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import CreateOwnerDto from './dto/create-owner.dto';
 import { UpdateOwnerDto } from './dto/update-owner.dto';
+import { Owner } from './entities/owner.entity';
 
 @Injectable()
 export class OwnerService {
-  create(createOwnerDto: CreateOwnerDto) {
-    return 'This action adds a new owner';
+  constructor(
+    @InjectRepository(Owner)
+    private ownerRepository: Repository<Owner>
+  ) {}
+  async create(ownerData: CreateOwnerDto) {
+    const newOwner = await this.ownerRepository.create(ownerData);
+    await this.ownerRepository.save(newOwner);
+    return newOwner;
   }
 
   findAll() {
