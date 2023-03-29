aa47bbdf24cce79a9bd1d6abeeb0ea71c3c8e993
Your Name <you@example.com>
2023-03-29 12:51:41 +0200

    Delete account

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Body, Delete, Get, HttpException, HttpStatus, Injectable, Param, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import CreateAccountDto from './dto/create-account.dto';
@@ -12,12 +12,14 @@ export class AccountService {
     @InjectRepository(Account)
     private accountRepository: Repository<Account>
   ) {}
-  async create(accountData: CreateAccountDto) {
+  @Post()
+  async create(@Body() accountData: CreateAccountDto) {
     const newAccount = await this.accountRepository.create(accountData);
     await this.accountRepository.save(newAccount)
     return newAccount;
   }
 
+  @Get('/accont/:id')
   async getById(id: number) {
     const account = await this.accountRepository.findOne({where: { id }});
     if (account) {
@@ -30,7 +32,8 @@ export class AccountService {
     return `This action updates a #${id} account`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} account`;
+  @Delete('/account/:id')
+  remove(@Param('id') id: number) {
+    return this.accountRepository.remove(Account[+id])
   }
 }
